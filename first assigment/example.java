77145e85788cd0a11ae976343442bb17599fb509
mostafa esam <mostafaesam3001@gmail.com>
2024-03-19 14:58:09 +0200

    all lab of java

---
+++ first assigment/example.java
@@ -0,0 +1,16 @@
+// public class example{
+//     Public Static void Main(String [] args){
+
+//         System.out.println("hello world");
+//     }
+
+
+// }
+
+public class example {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    
+  }
+}
+
---
+++ first assigment/example2.java
@@ -0,0 +1,12 @@
+public class example2{
+    public static void main(String[] args){
+     if(args[0].equals ("mostafa"))
+     {
+    
+        System.out.println(args[0] + " helloo ya darsh");}else{
+        System.out.println("invalid input");
+
+     }
+
+    }
+}
